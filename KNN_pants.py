5ec272207f208ce51173c9d140a9ada8c4195ae7
Niclas Carlsson <niclas.carlsson@student.kyh.se>
2022-04-14 12:31:04 +0200

    My KNN, SKlearn KNN and SKlearn Decision Tree working!

--- KNN_pants.py
+++ KNN_pants.py
@@ -1,4 +1,8 @@
 import math
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from pants_data_male import pants_data_m
 from pants_data_female import pants_data_f
 from collections import Counter
@@ -40,6 +44,38 @@ distances.sort(key=lambda data: data['dist'])
 distances = distances[:check_nearest]
 
 counter = Counter([d['size'] for d in distances])
+print('*'*40)
 print(check_nearest, 'nearest neighbours')
 print(counter.most_common(check_nearest))
+print('*'*40)
 print('My KNN predicts this pants size for you: ', counter.most_common(1)[0][0].upper())
+print('*'*40)
+
+X = None
+y = None
+if user_sex == 0:
+    X = list(zip(pants_data_m['stature'], pants_data_m['weightkg']))
+    y = list(pants_data_m['size'])
+else:
+    X = list(zip(pants_data_f['stature'], pants_data_f['weightkg']))
+    y = list(pants_data_f['size'])
+
+model = KNeighborsClassifier(n_neighbors=check_nearest)
+model.fit(X, y)
+predicted = model.predict([user_data])
+print('SKlearn-KNN predicts this pants size for you: ', predicted)
+print('*' * 40)
+
+if user_sex == 0:
+    X = list(zip(pants_data_m['stature'], pants_data_m['weightkg']))
+    y = list(pants_data_m['size'])
+else:
+    X = list(zip(pants_data_f['stature'], pants_data_f['weightkg']))
+    y = list(pants_data_f['size'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+clf = DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+y_pred = clf.predict([[user_length, user_weight]])
+print('SKlearn Decision Tree Classifier predicts this t-shirt size for you:', y_pred)
+print('*' * 40)
--- KNN_tshirt.py
+++ KNN_tshirt.py
@@ -1,4 +1,7 @@
 import math
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from tshirt_data_male import new_data_m
 from tshirt_data_female import new_data_f
 from collections import Counter
@@ -20,18 +23,18 @@ user_weight = int(input("Enter weight in hg:\n"))
 user_data = [user_length, user_weight]
 
 distances = []
-pants_data = []
+tshirt_data = []
 sizes = []
 
 if user_sex == 0:
-    pants_data = list(zip(new_data_m['stature'], new_data_m['weightkg']))
+    tshirt_data = list(zip(new_data_m['stature'], new_data_m['weightkg']))
     sizes = new_data_m['size']
 else:
-    pants_data = list(zip(new_data_f['stature'], new_data_f['weightkg']))
+    tshirt_data = list(zip(new_data_f['stature'], new_data_f['weightkg']))
     sizes = new_data_f['size']
 
 check_nearest = int(input('Enter K value: '))
-for i, df in enumerate(pants_data):
+for i, df in enumerate(tshirt_data):
     dist = euclidian_distance(df, user_data)
     distances.append({'distance': dist, 'size': sizes[i]})
 
@@ -40,7 +43,38 @@ distances.sort(key=lambda data: data['distance'])
 distances = distances[:check_nearest]
 
 counter = Counter([d['size'] for d in distances])
-m_common = counter.most_common()
+print('*' * 40)
 print(check_nearest, 'nearest neighbours')
 print(counter.most_common(check_nearest))
+print('*' * 40)
 print('My KNN predicts this t-shirt size for you: ', counter.most_common(1)[0][0].upper())
+print('*' * 40)
+
+X = None
+y = None
+if user_sex == 0:
+    X = list(zip(new_data_m['stature'], new_data_m['weightkg']))
+    y = list(new_data_m['size'])
+else:
+    X = list(zip(new_data_f['stature'], new_data_f['weightkg']))
+    y = list(new_data_f['size'])
+
+model = KNeighborsClassifier(n_neighbors=check_nearest)
+model.fit(X, y)
+predicted = model.predict([user_data])
+print('SKlearn-KNN predicts this t-shirt size for you: ', predicted)
+print('*' * 40)
+
+if user_sex == 0:
+    X = list(zip(new_data_m['stature'], new_data_m['weightkg']))
+    y = list(new_data_m['size'])
+else:
+    X = list(zip(new_data_f['stature'], new_data_f['weightkg']))
+    y = list(new_data_f['size'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+clf = DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+y_pred = clf.predict([[user_length, user_weight]])
+print('SKlearn Decision Tree Classifier predicts this t-shirt size for you:', y_pred)
+print('*' * 40)
